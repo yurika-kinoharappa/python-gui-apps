7edb936dcb51008b5183640f1407d4f3a18d4e30
yurika-kinoharappa <yuyuririka64502@gmail.com>
2024-06-26 10:55:43 +0900

    work4.py

--- work4.py
+++ work4.py
@@ -9,10 +9,37 @@ fg_color = "#FFFFFF"  # 白
 window.configure(bg=bg_color)
 # ↑↑↑ お約束のコード ↑↑↑
 
+# 出力ラベルの作成
+label0 = tk.Label(window, text="名前を入力してください", bg=bg_color, fg=fg_color)
+label0.pack(pady=10)
+
+name_list = []
+
 
 def button_action():  # 関数の定義 ※ボタンが押されたときの動き
     user_input = entry1.get()  # 入力値を取得
-    label1.config(text=f"Hello,{user_input}！")  # 画面に出力
+    name_list.append(user_input)
+
+    formatted_str = ""
+    for name in name_list:
+        formatted_str += f"{name}\n"
+        # formatted_str = "\n".join(name)
+
+    label1.config(text=formatted_str)
+
+
+def button_action2():  # 関数の定義 ※ボタンが押されたときの動き
+    user_input = entry1.get()  # 入力値を取得
+    name_list.append(user_input)
+
+    import random
+
+    num1 = len(name_list)
+    num2 = random.randint(0, num1 - 1)
+    label2.config(text=name_list[num2])
+
+
+# 画面に出力
 
 
 # 入力フィールドの作成
@@ -20,12 +47,20 @@ entry1 = tk.Entry(window, bg=fg_color, fg=bg_color)
 entry1.pack(pady=10)
 
 # ボタンの作成
-button1 = tk.Button(window, text="Submit", command=button_action)
+button1 = tk.Button(window, text="追加", command=button_action)
 button1.pack(pady=10)
 
-# 出力ラベルの作成
+
 label1 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
 label1.pack(pady=10)
+
+# ボタンの作成
+button2 = tk.Button(window, text="ランダム選択", command=button_action2)
+button2.pack(pady=10)
+
+
+label2 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
+label2.pack(pady=10)
 
 # ↓↓↓ お約束のコード ↓↓↓
 window.mainloop()
